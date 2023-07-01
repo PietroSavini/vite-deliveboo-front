70f7a3d44e9ffe2505e356b970cf2c4d5c67954e
PietroSavini <savinipietro@gmail.com>
2023-07-01 14:17:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss">
+@use "./styles/style.scss";
+@use "./styles/partials/root.scss" as *;
+div{
+  background-color: $primary_color;
+}
+</style>
