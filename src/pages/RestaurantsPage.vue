74115e11fa8a5c4b5214eff52d03759221e85c3d
yaredghebre <yared2391@gmail.com>
2023-07-06 19:46:14 -0300

    Add restaurant component, started rest card

--- src/pages/RestaurantsPage.vue
+++ src/pages/RestaurantsPage.vue
@@ -1,21 +1,47 @@
 <script >
+// import axios from "axios";
 import { store } from "../store.js";
+import SectionJumbo from "../components/SectionJumbo.vue";
+import RestaurantCard from "../components/RestaurantCard.vue";
 
 export default {
   name: "RestaurantsPage",
+  components: {
+    SectionJumbo,
+    RestaurantCard,
+  },
   data() {
     return {
       store,
-    }
-  }
+      restaurants: [],
+    };
+  },
+//   mounted() {
+//     this.getRestaurants();
+//   },
+//   methods: {
+//     getRestaurants() {
+//         axios.get(`${store.ApiRestaurantsUrl}`).then((resp) => {
+//             console.log(resp);
+//             // this.restaurants
+//         });
+//     }
+//   }
 }
-
 </script>
 
 <template>
-    <div class="container">
-        <h1 class="mt-5">Questa è la pagina dei ristoranti</h1>
-    </div>
+    <section class="jumbo-section">
+        <SectionJumbo :text="`Restaurants Page`"/>
+
+        <div class="text-center my-5">
+            <RestaurantCard />
+        </div>
+    </section>
+
+    <main>
+
+    </main>
 
 </template>
 
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {reactive} from 'vue';
 
 export const store = reactive({
     ApiTypesUrl : 'http://localhost:8000/api/types',
+    ApiRestaurantsUrl: "http://localhost:8000/api/restaurants",
 })
 
 
