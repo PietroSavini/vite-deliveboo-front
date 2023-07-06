bbb2cb1b0586eccb1868c8404211c02629491eaa
PietroSavini <savinipietro@gmail.com>
2023-07-06 18:38:37 +0200

    ADD: scaffholding foe routerlinks

--- src/App.vue
+++ src/App.vue
@@ -18,30 +18,13 @@ export default{
 
 <template>
 <AppHeader/>
- <section class="hero">
-    <video autoplay loop muted >
-      
-      <source src="./assets/DeliveBoo_edited.mp4" type="video/mp4">
-    </video>
- </section>
+<router-view/>
 </template>
 
 <style lang="scss">
 @use "./styles/style.scss";
 @use "./styles/partials/root.scss" as *;
 
-.hero{
-  height: 45vh;
-  border: 1px solid black;
-  position: relative;
-  video{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-}
+
 
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<script >
+import {store} from "./store.js";
+
+
+export default{
+  data(){
+    return{
+      store,
+    }
+  },
+  components:{
+   
+  }
+}
+
+</script>
+
+<template>
+ <section class="hero">
+    <video autoplay loop muted >
+      <source src="./assets/DeliveBoo_edited.mp4" type="video/mp4">
+    </video>
+ </section>
+</template>
+
+<style lang="scss">
+.hero{
+  height: 45vh;
+  border: 1px solid black;
+  position: relative;
+  video{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
