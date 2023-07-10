44ec8dabec33a471da188cc23d5747524f33fd8e
PietroSavini <savinipietro@gmail.com>
2023-07-10 21:49:01 +0200

    ADD: responsive layout on HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -82,15 +82,88 @@ export default {
 
 <template>
   <HomePageJumbo @getRestaurants="getRestaurants" />
-  <div class="ms_col" v-for="restaurant in restaurantsTypes" :key="restaurant.name">
-    <RestaurantCard :restaurant="restaurant" />
-  </div>
-  <Pagination v-if="restaurantsTypes.length > 0" class="mb-4" :currentPage="currentPage" :lastPage="lastPage"
-    @changePage="getRestaurantsPage" />
+  
+  
 
-  <HomePageMain class="mt-4" />
+  
+  <HomePageMain v-if="restaurantsTypes.length === 0" />
+  <section class="featured" v-if="restaurantsTypes.length > 0">
+    <div class="background-extencion"></div> 
+    <div class="container">
+        <div class="section-title">
+            <div class="img-absolute">
+                <img src="https://res.cloudinary.com/glovoapp/f_auto,h_48,w_48,q_auto:best/CX/new_backend_driven_home_screen/newUserRecommendationsWidgetIconLight" alt="">
+            </div>   
+            <h5>in base alle categorie scelte</h5>
+        </div>
+        <div class="row">
+            <div class="ms_col" v-for="restaurant in restaurantsTypes" :key="restaurant.name">
+                <RestaurantCard :restaurant="restaurant" />
+            </div>
+            <Pagination v-if="restaurantsTypes.length > 0" class="mb-4" :currentPage="currentPage" :lastPage="lastPage" @changePage="getRestaurantsPage" />
+        </div>
+    </div>
+</section>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/root.scss" as *;
+.featured{
+    background-color: $secondary_color;
+    padding:1.5rem 0;
+    position: relative;
+    
+    
+    
+    .background-extencion{
+        
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        top: -25px;
+        height: 50px;
+        border-radius: 100%;
+        
+        background-color: $secondary_color ;
+    }
+    .container{
+        
+        
+        .section-title{
+            color: black;
+            @media screen and (max-width : 768px) {
+                padding-left: 37%;
+            }
+            
+            .img-absolute{
+                position: relative;
+            }
+            
+            img{
+                position: absolute;
+                z-index: 0;
+                top: -15px;
+                left: -50px;
+                transform: rotate(25deg);
+            }
+            
+            h5{
+                position: relative;
+                z-index: 1;
+                font-weight: bold;
+                
+            }
+        }
+        .row{
+            flex-wrap: wrap;
+            justify-content: center;
+            
+            .ms_col{
+                width: 400px;
+                height: 220px;
+                padding: 5px 5px;
+            }
+        }
+    }
+}
 </style>
